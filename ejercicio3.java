1b6c40335ba7643736252375d363910601335af0
DavidMarLorInc <david.marmor93@gmail.com>
2018-04-20 11:22:15 +0200

    Ejercicio 3, falta incluir la implementacion

---
+++ ejercicio3.java
@@ -0,0 +1,10 @@
+class ejercicio3{
+    public static void main(String args[])
+    {
+        String nombre="David";
+
+        System.out.println("Bienvenido, "+nombre);
+
+        //Ampliar preguntado el nombre que se encuentra en el ordenador y publicarlo.
+    }
+}
