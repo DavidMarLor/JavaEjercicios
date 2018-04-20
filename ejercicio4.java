a5bda9a5c9b27f180f2bfe454804775f7c867484
DavidMarLorInc <david.marmor93@gmail.com>
2018-04-20 11:27:47 +0200

    Falta realizar la ampliacion con JOptionPane

---
+++ ejercicio4.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class ejercicio4{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce tu nombre");
+        String nombre = sc.nextLine();
+
+        System.out.println("Bienvenido "+nombre);
+    }
+}
+
+//Ampliacion modificarlo para usar la opcion de JOptionPane.
