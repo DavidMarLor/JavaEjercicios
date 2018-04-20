b69fdd9f9569b00add74ecb026cb7d346f76b5a1
DavidMarLorInc <david.marmor93@gmail.com>
2018-04-20 11:20:57 +0200

    ejercicio 1, leer ampliacion

---
+++ ejercicio1.java
@@ -0,0 +1,31 @@
+class ejercicio1{
+    public static void main(String[] args)
+    {
+        int num1 = 10;
+        int num2 = 15;
+
+        int suma = num1 + num2;
+        int resta = num2 - num1;
+        int multiplicacion = num1 * num2;
+        int division = num2 / num1;
+        int modulo = num2 % num1;
+
+        System.out.println("El resultado de la suma es: ");
+        System.out.println(suma);
+        System.out.println("El resultado de la resta es: ");
+        System.out.println(resta);
+        System.out.println("El resultado de la multiplicacion es: ");
+        System.out.println(multiplicacion);
+        System.out.println("El resultado de la division es: ");
+        System.out.println(division);
+        System.out.println("El resultado del modulo es: ");
+        System.out.println(modulo);
+
+        System.out.println("El resultado de la suma es: "+suma);//De esta forma imprimimos en una sola linea
+
+        //Ampliar, modificarlo para que pida las variables por pantalla.
+
+        //Como imprimir con un solo print el resultado y la frase.
+        //println, imprime con un salto de linea es como en c \n.
+    }
+}
