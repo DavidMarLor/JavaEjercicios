ce4183923b8ae95580d4320963620e87780f2069
DavidMarLorInc <david.marmor93@gmail.com>
2018-04-21 16:05:31 +0200

    Ejercicio 10, la ampliacion es el ejercicio 11, que consiste en usar un bucle for

---
+++ ejercicio10.java
@@ -0,0 +1,14 @@
+//Ejercicio 10. Mostrar por pantalla los numeros del 1 al 100 usando un bucle while.
+
+class ejercicio10{
+    public static void main(String[] args)
+    {
+        int contador = 1;
+
+        while(contador<=100)
+        {
+            System.out.println("El numero es: "+contador);
+            contador++;
+        }
+    }
+}
