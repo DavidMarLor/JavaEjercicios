73b1bff90072b9b0af80ce822d83c451be509b5f
DavidMarLorInc <david.marmor93@gmail.com>
2018-04-22 18:11:24 +0200

    Ejercicio 11

---
+++ ejercicio11.java
@@ -0,0 +1,11 @@
+//Ejercicio 11, mostrar por pantalla los numeros del 1 al 100 usando un bucle for
+
+class ejercicio11{
+    public static void main(String[] args)
+    {
+        for(int i=1; i<=100; i++)
+        {
+            System.out.println("El numero es: "+i);
+        }
+    }
+}
